61091b63563d81d72e7fa08984978e9567413894
toni <toniliu672@gmail.com>
2024-07-25 10:17:06 +0800

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import TopologiJaringan from './pages/TopologiJaringan';
 
 function App() {
-  return <TopologiJaringan />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/topologijaringan" />} />
+        <Route path="/topologijaringan" element={<TopologiJaringan />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
